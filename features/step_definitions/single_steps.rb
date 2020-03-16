9dcc2ce27c19022f45052a387d9b2729a7185a3b
Pierre Edouard Mathieu <pemathieu@hubee.corp>
2020-03-16 11:40:59 +0100

    tidying

--- features/step_definitions/single_steps.rb
+++ features/step_definitions/single_steps.rb
@@ -1,13 +1,13 @@
+When /^I discover$/ do
+  click_button('Discover')
+end
+
+Then /^I should see homepage do
+  expect(page).to have_content("TV channels (Square)")
+end
+
 When /^I fill in "([^\"]*)" found by "([^\"]*)" with "([^\"]*)"$/ do |value, _type, keys|
   fill_in(value, with: keys)
-end
-
-When /^I submit$/ do
-  find_field('q').native.send_key(:enter)
-end
-
-Then /^I should see title "([^\"]*)"$/ do |title|
-  expect(page).to have_title title
 end
 
 Then /^I should see no result do
@@ -16,12 +16,4 @@ end
 
 Then /^I should see flash results do
   expect(page).to have_content("10 results found in total")
-end
-
-When /^I discover$/ do
-  click_button('Discover')
-end
-
-Then /^I should see homepage do
-  expect(page).to have_content("TV channels (Square)")
 end
