7d9d39303e207a4444608d3c18d3f78e40d6e5d8
Pierre Edouard Mathieu <pemathieu@hubee.corp>
2020-03-16 11:57:52 +0100

    simplify

--- features/single.feature
+++ features/single.feature
@@ -7,13 +7,4 @@ Background:
 Scenario: Can discover
   When I discover
   Then I should see homepage
-
-
-Scenario: Can not find search results
-  When I fill in "Search" found by "placeholder" with "flashdadada"
-  Then I should see no result
-
-
-Scenario: Can find search results
-  When I fill in "Search" found by "placeholder" with "flash"
-  Then I should see flash results 
+  
--- features/step_definitions/single_steps.rb
+++ features/step_definitions/single_steps.rb
@@ -5,15 +5,3 @@ end
 Then /^I should see homepage do
   expect(page).to have_content("TV channels (Square)")
 end
-
-When /^I fill in "([^\"]*)" found by "([^\"]*)" with "([^\"]*)"$/ do |value, _type, keys|
-  fill_in(value, with: keys)
-end
-
-Then /^I should see no result do
-  expect(page).to have_content("No results")
-end
-
-Then /^I should see flash results do
-  expect(page).to have_content("10 results found in total")
-end
